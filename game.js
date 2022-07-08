0b4be7f40bfc49da5bfbaade745916ebea1bfd23
GS Kariyapperuma <gayathmikariyapperuma@gmail.com>
2022-07-08 12:57:10 +0530

    update

--- game.js
+++ game.js
@@ -66,23 +66,32 @@ getNewQuestion = () => {
 };
 
 choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    if (!acceptingAnswers) return;
-
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
-
-    const classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-    selectedChoice.parentElement.classList.add(classToApply);
-
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
+    choice.addEventListener("click", e => {
+      if (!acceptingAnswers) return;
+  
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
+  
+      const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+  
+      if (classToApply === "correct") {
+        incrementScore(CORRECT_BONUS);
+      }
+  
+      selectedChoice.parentElement.classList.add(classToApply);
+  
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 1000);
+    });
   });
-});
-
-startGame();
+  
+  incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
+  
+  startGame();
